8ec351a9851f42830b7e65d66b3bf47da85793dc
Seiya Asahina <seiya@asahina>
2024-02-06 21:29:20 +0900

    add

---
+++ bbs/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.urls import reverse_lazy
+
+import stripe
+
+stripe.api_key  = settings.STRIPE_API_KEY
+
+
+class IndexView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "bbs/index.html")
+
+index   = IndexView.as_view()
+
+class CheckoutView(LoginRequiredMixin,View):
+    def post(self, request, *args, **kwargs):
+
+        # セッションを作る
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': settings.STRIPE_PRICE_ID,
+                    'quantity': 1,
+                },
+            ],
+            payment_method_types=['card'],
+            mode='subscription',
+            success_url=request.build_absolute_uri(reverse_lazy("bbs:success")) + '?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=request.build_absolute_uri(reverse_lazy("bbs:index")),
+        )
+
+        # セッションid
+        print( checkout_session["id"] )
+
+        return redirect(checkout_session.url)
+
+checkout    = CheckoutView.as_view()
+
+class SuccessView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+
+        # パラメータにセッションIDがあるかチェック
+        if "session_id" not in request.GET:
+            print("セッションIDがありません。")
+            return redirect("bbs:index")
+
+
+        # そのセッションIDは有効であるかチェック。
+        try:
+            checkout_session_id = request.GET['session_id']
+            checkout_session    = stripe.checkout.Session.retrieve(checkout_session_id)
+        except:
+            print( "このセッションIDは無効です。")
+            return redirect("bbs:index")
+
+        print(checkout_session)
+
+        # statusをチェックする。未払であれば拒否する。(未払いのsession_idを入れられたときの対策)
+        if checkout_session["payment_status"] != "paid":
+            print("未払い")
+            return redirect("bbs:index")
+
+        print("支払い済み")
+
+
+        # 有効であれば、セッションIDからカスタマーIDを取得。ユーザーモデルへカスタマーIDを記録する。
+        request.user.customer   = checkout_session["customer"]
+        request.user.save()
+
+        print("有料会員登録しました！")
+
+        return redirect("bbs:index")
+
+success     = SuccessView.as_view()
+
+
+# サブスクリプションの操作関係
+class PortalView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+
+        if not request.user.customer:
+            print( "有料会員登録されていません")
+            return redirect("bbs:index")
+
+        # ユーザーモデルに記録しているカスタマーIDを使って、ポータルサイトへリダイレクト
+        portalSession   = stripe.billing_portal.Session.create(
+            customer    = request.user.customer,
+            return_url  = request.build_absolute_uri(reverse_lazy("bbs:index")),
+        )
+
+        return redirect(portalSession.url)
+
+portal      = PortalView.as_view()
+
+
+class PremiumView(View):
+    def get(self, request, *args, **kwargs):
+        
+        # カスタマーIDを元にStripeに問い合わせ
+        try:
+            subscriptions = stripe.Subscription.list(customer=request.user.customer)
+        except:
+            print("このカスタマーIDは無効です。")
+            return redirect("bbs:index")
+        
+        # ステータスがアクティブであるかチェック。
+        for subscription in subscriptions.auto_paging_iter():
+            if subscription.status == "active":
+                print("サブスクリプションは有効です。")
+
+                return render(request, "bbs/premium.html")
+            else:
+                print("サブスクリプションが無効です。")
+
+        return redirect("bbs:index")
+
+premium     = PremiumView.as_view()
+
+
+
